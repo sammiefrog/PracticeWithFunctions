6704e0d5e73fa93fca5def2adfcb1350f57dc599
sammiefrog <sammiefrog@live.com>
2021-04-29 15:05:14 -0500

    adds solved folder with solutions to exercises

---
+++ Solved/script.js
@@ -0,0 +1,135 @@
+//1. Write code in the body of each function so that:
+// * `add` returns the result of `num1` plus `num2`.
+// * `subtract` returns the result of `num1` minus `num2`.
+// * `multiply` returns the result of `num1` times `num2`.
+// * `divide` returns the result of `num1` divided by `num2`.
+
+//Check all your work with console.log() so the answers are displayed in the browser!
+// ** BONUS ** convert all of these into Arrow Functions
+
+const add = (num1, num2) => num1 + num2;
+
+const subtract = (num1, num2) => num1 - num2;
+
+const multiply = (num1, num2) => num1 * num2;
+
+const divide = (num1, num2) => num1 / num2;
+
+console.log(add(4, 5));
+console.log(subtract(9, 5));
+console.log(multiply(3, 7));
+console.log(divide(10, 2));
+
+
+
+
+//2. Write a FUNCTION to print each item in the following array to the console
+
+const fruits = ["Apple", "Banana", "Pear", "Strawberry", "Canteloupe"];
+
+const printFruits = () => {
+    for (let i = 0; i < fruits.length; i++){
+        console.log(fruits[i]);
+    }
+}
+
+printFruits();
+
+
+//3. This one has three steps, use Javascript Array methods for each part (try w3Schools for help)
+
+let planets = ["Earth", "Saturn", "Mars", "Jupiter", "Uranus", "Venus"];
+
+// Use an array method to remove "Venus" from the planets array, then print the changed array to the console
+planets.pop();
+console.log(planets);
+
+// Use an array method to add "Mercury" to the beginning of the planets array, then print the changed array to the console
+planets.unshift("Mercury");
+console.log(planets);
+
+// Use an array method to join the following array with the planets array, then print the new array to the console
+let morePlanets = ["Pluto", "Neptune"];
+
+console.log(planets.concat(morePlanets));
+
+
+
+
+//4. Write code within the body of the following function to print all positive numbers from 1 to `num`, and console.log the result. (Hint: when you call this function to test it, don't forget to pass in a positive number as an argument)
+
+const logNums = (num) => {
+      for (var i = 1; i <= num; i++) {
+        console.log(i);
+      }
+};
+
+logNums(6);
+
+//5. Create a function that accepts two parameters, num1 and num2. If num1 is greater than num2 print "First one wins!", if num1 is equal to num 2 print "It's a draw!", if num1 is less than num2 print "Second number prevails!". Test the function three times so each condition is met successfully.
+
+const checkNumbers = (num1, num2) => {
+    if (num1 > num2) {
+        console.log("First number wins!");
+    } else if (num1 === num2) {
+        console.log("It's a draw!");
+    } else {
+        console.log("Second number prevails!");
+    }
+};
+
+checkNumbers(17, 11);
+checkNumbers(9, 9);
+checkNumbers(4, 10);
+
+//Bonus Challenge!
+//Write code inside the following function that accepts a string and returns `true` is the string is a palindrome, and `false` if the string is not a palindrome.
+// --- A Palindrome is a word that is spelled the same way forwards and backwards ---
+//Examples of palindromes = mom, dad, level, radar
+
+const isPalindrome = (str) => {
+//write code here
+      let reversedString = str.split("").reverse().join("");
+
+      if (reversedString === str) {
+        return true;
+      } else {
+        return false;
+      }
+    
+};
+
+console.log(isPalindrome("racecar")); //expects true
+console.log(isPalindrome("elephant")); //expects false
+
+// Others solutions from class:
+
+// const isPalindrome = (string) => {
+// //write code here
+//     let x = string.length - 1
+//     for (let i = 0; i < Math.floor(string.length / 2); i++){
+//         if (string[i] !== string[x]) {
+//             return false;
+//         }
+//         x -= 1
+//     }
+//     return true;
+// };
+
+// const isPalindrome = string => {
+//     let splitString = string.split("");
+//     console.log(splitString);
+
+//     let reverseString = splitString.reverse();
+//     console.log(reverseString);
+
+//     let joinString = reverseString.join('');
+//     console.log(joinString);
+
+//     if (joinString === string) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
