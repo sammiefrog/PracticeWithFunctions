2ac5c0e82d8c405c87f38b583c4bef9901009771
James <jamesogordon0@gmail.com>
2021-04-29 14:17:08 -0500

    Ran out of time - Almost Finished

--- assetts/script.js
+++ assetts/script.js
@@ -10,20 +10,38 @@ console.log("Let's try out our JavaScript Skills!");
     //Check all your work with console.log() so the answers are displayed in the browser!
     // ** BONUS ** convert all of these into Arrow Functions
 
-function add(num1, num2) {};
+let num1 = 1;
+let num2 = 3;
 
-function subtract(num1, num2) {};
+function add(num1, num2) {
+    return num1 + num2;
+};
 
-function multiply(num1, num2) {};
+function subtract(num1, num2) {
+    return num1 - num2;
+};
 
-function divide(num1, num2) {};
+function multiply(num1, num2) {
+    return num1 * num2;
+};
+
+function divide(num1, num2) {
+    return num1 / num2;
+};
+
+console.log(add(num1, num2));
+console.log(subtract(num1, num2));
+console.log(multiply(num1, num2));
+console.log(divide(num1, num2));
 
 
 //2. Write a FUNCTION to print each item in the following array to the console
 
 const fruits = ['Apple', 'Banana', 'Pear', 'Strawberry', 'Canteloupe'];
 
-
+for(let i=0; i < fruits.length; i++){
+    console.log(fruits[i])
+};
 
 
 
@@ -32,20 +50,30 @@ const fruits = ['Apple', 'Banana', 'Pear', 'Strawberry', 'Canteloupe'];
 let planets = ["Earth", "Saturn", "Mars", "Jupiter", "Uranus", "Venus"];
 
 // Use an array method to remove "Venus" from the planets array, then print the changed array to the console
+console.log(planets.pop())
 
 // Use an array method to add "Mercury" to the beginning of the planets array, then print the changed array to the console
 
+console.log(planets.push('Mercury'))
+
 // Use an array method to join the following array with the planets array, then print the new array to the console
 let morePlanets = ["Pluto", "Neptune"];
+
+Array.prototype.push.apply(planets, morePlanets)
+
+console.log(planets)
 
 
 
 //4. Write code within the body of the following function to print all positive numbers from 1 to `num`, and console.log the result. (Hint: when you call this function to test it, don't forget to pass in a positive number as an argument)
 
 const logNums = num => {
-
+ for (i=1; i<=100; i++)
+    console.log(logNums())
 }
 
+
+logNums()
 
 
 //5. Create a function that accepts two parameters, num1 and num2. If num1 is greater than num2 print "First one wins!", if num1 is equal to num 2 print "It's a draw!", if num1 is less than num2 print "Second number prevails!". Test the function three times so each condition is met successfully.
@@ -67,3 +95,5 @@ const isPalindrome = (string) => {
 
 isPalindrome("racecar"); //expects true
 isPalindrome("elephant"); //expects false
+
+// Gaoussou Terrance Cleo James
