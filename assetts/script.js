f5bce5d71a73c4208b3ed3add54058e85bd8dc79
aabiye <aabiye7@gmail.com>
2021-05-08 20:44:04 -0500

    answer to questions

--- assetts/script.js
+++ assetts/script.js
@@ -2,28 +2,63 @@ console.log("Let's try out our JavaScript Skills!");
 
 
 //1. Write code in the body of each function so that:
-    // * `add` returns the result of `num1` plus `num2`.
-    // * `subtract` returns the result of `num1` minus `num2`.
-    // * `multiply` returns the result of `num1` times `num2`.
-    // * `divide` returns the result of `num1` divided by `num2`.
+// * `add` returns the result of `num1` plus `num2`.
+// * `subtract` returns the result of `num1` minus `num2`.
+// * `multiply` returns the result of `num1` times `num2`.
+// * `divide` returns the result of `num1` divided by `num2`.
 
-    //Check all your work with console.log() so the answers are displayed in the browser!
-    // ** BONUS ** convert all of these into Arrow Functions
+//Check all your work with console.log() so the answers are displayed in the browser!
+// ** BONUS ** convert all of these into Arrow Functions
 
-function add(num1, num2) {};
+// function add(num1, num2) {
 
-function subtract(num1, num2) {};
+//     return num1 + num2;
+// };
 
-function multiply(num1, num2) {};
 
-function divide(num1, num2) {};
+const add = (num1, num2) => num1 + num2;
+
+console.log(add(10, 5));
+
+function subtract(num1, num2) {
+    return num1 - num2;
+};
+
+console.log(subtract(10, 5));
+
+
+function multiply(num1, num2) {
+    return num1 * num2;
+};
+
+console.log(multiply(10, 5));
+
+
+function divide(num1, num2) {
+    return num1 / num2;
+};
+
+console.log(divide(10, 5));
+
 
 
 //2. Write a FUNCTION to print each item in the following array to the console
 
 const fruits = ['Apple', 'Banana', 'Pear', 'Strawberry', 'Canteloupe'];
 
+// function allfruits() {
+//     for (let i = 0; i < fruits.length; i++) {
+//         console.log(fruits[i]);
+//     }
+// }
 
+const allfruits = () => {
+    for (let i = 0; i < fruits.length; i++) {
+        console.log(fruits[i]);
+    }
+}
+
+allfruits();
 
 
 
@@ -33,23 +68,52 @@ let planets = ["Earth", "Saturn", "Mars", "Jupiter", "Uranus", "Venus"];
 
 // Use an array method to remove "Venus" from the planets array, then print the changed array to the console
 
+planets.pop();
+console.log(planets);
+
+
 // Use an array method to add "Mercury" to the beginning of the planets array, then print the changed array to the console
+
+planets.unshift("Mercury");
+console.log(planets);
 
 // Use an array method to join the following array with the planets array, then print the new array to the console
 let morePlanets = ["Pluto", "Neptune"];
-
+console.log(planets.concat(morePlanets));
 
 
 //4. Write code within the body of the following function to print all positive numbers from 1 to `num`, and console.log the result. (Hint: when you call this function to test it, don't forget to pass in a positive number as an argument)
 
 const logNums = num => {
-
+    for (let i = 1; i <= num; i++) {
+        console.log(i);
+    }
 }
-
+logNums(4);
 
 
 //5. Create a function that accepts two parameters, num1 and num2. If num1 is greater than num2 print "First one wins!", if num1 is equal to num 2 print "It's a draw!", if num1 is less than num2 print "Second number prevails!". Test the function three times so each condition is met successfully.
 
+const myNum = (num1, num2) => {
+    if (num1 > num2) {
+        console.log("First one wins!");
+
+    }
+
+    else if (num1 === num2) {
+        console.log("It's a draw!");
+
+    }
+
+    else {
+        console.log("Second number prevails!");
+    }
+
+}
+
+myNum(5, 5);
+myNum(10, 5);
+myNum(2, 5);
 
 
 
@@ -60,9 +124,9 @@ const logNums = num => {
 // Hint: you can use multiple javascript array methods chained together, check w3schools for ideas
 
 const isPalindrome = (string) => {
-//write code here
-    
-    
+    //write code here
+
+
 };
 
 isPalindrome("racecar"); //expects true
